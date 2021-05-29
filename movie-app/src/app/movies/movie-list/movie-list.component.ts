f448c3416b93a754e28e23a55ef80ba1b0a1a169
robertlongmendez <robertlongmendez@gmail.com>
2021-05-29 11:00:29 -0500

    Fixing movie-detail component
    Had method selectedMovie typed incorrectly

--- movie-app/src/app/movies/movie-list/movie-list.component.ts
+++ movie-app/src/app/movies/movie-list/movie-list.component.ts
@@ -10,9 +10,9 @@ import { Movie } from '../movie.model';
 export class MovieListComponent implements OnInit {
   @Output() movieWasSelected = new EventEmitter<Movie>();
   movies: Movie[] = [
-    new Movie('A Test Movie', 'This is definitely the end of the world type movie', 'https://townsquare.media/site/442/files/2016/06/action-posters-featured.jpg?w=980&q=75'),
-    new Movie('A Test Movie', 'This is definitely the end of the world type movie', 'https://townsquare.media/site/442/files/2016/06/action-posters-featured.jpg?w=980&q=75'),
-    new Movie('A Test Movie', 'This is definitely the end of the world type movie', 'https://townsquare.media/site/442/files/2016/06/action-posters-featured.jpg?w=980&q=75')
+    new Movie('Attack of the Killer Tomatoes', 'Aaargh!...', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrvQEBxBu-xAvpeHP6Srio3VI2iXj26vLC5BnM8qGETOezQ4ergsY60jlpWpn56XI8sTQ&usqp=CAU'),
+    new Movie('Arcade', 'The game wants to play with you.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6nUdtA3UVAPClm30Jz2TPzNVrijb6wrpiF0ciY5NJ3QEqbwmPcwRma0_xzBTpxIzBOiY&usqp=CAU'),
+    new Movie('The Giant Gila Monster', 'Pretty Straight Forward', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwLsLW26rNqWteyo7fIdRfB7-IuGvzXbLnF1zkQ5S1IcDoDrB2HWgyHedrah7vYHqWsQ8&usqp=CAU')
   ];
 
   constructor() { }

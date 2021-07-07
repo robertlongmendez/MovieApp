7bb985bb1ed2f690bf5c16cf3684d9b092babe37
robertlongmendez <82781708+robertlongmendez@users.noreply.github.com>
2021-07-07 11:14:42 -0500

    consolidated error handling into authObs
    corrected mistake of not adding return to the login method in authentication

--- movie-app/src/app/auth/auth.component.ts
+++ movie-app/src/app/auth/auth.component.ts
@@ -1,7 +1,9 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Router } from '@angular/router';
+import { Observable } from "rxjs";
 
-import { AuthService } from "./auth.service";
+import { AuthService, AuthResponseData } from "./auth.service";
 
 
 @Component ({
@@ -12,40 +14,44 @@ import { AuthService } from "./auth.service";
 export class AuthComponent {
 isLoginMode = true;
 isLoading = false;
-error: string;
+error: string = null;
 
-constructor(private authService: AuthService) {}
+constructor(private authService: AuthService, private router: Router) {}
 
 onSwitchMode() {
   this.isLoginMode = !this.isLoginMode;
 }
 
 onSubmit(form: NgForm) {
-if (!form.valid) {
-  return;
-}
-const email = form.value.email;
-const password = form.value.password;
-
-this.isLoading = true;
-if (this.isLoginMode) {
-  //..
-} else {
-  this.authService.signup(email, password).subscribe(resData => {
-  console.log(resData);
-  this.isLoading = false;
-},
-errorRes => {
-  console.log(errorRes);
-  switch (errorRes.error.error.message) {
-    case 'Email_EXISTS':
-      this.error = 'This email exists already';
+  if (!form.valid) {
+    return;
   }
-  // this.error = 'An error occurred!';
-  this.isLoading = false;
-}
-);
-}
+  const email = form.value.email;
+  const password = form.value.password;
+
+  let authObs: Observable<AuthResponseData>
+
+  this.isLoading = true;
+  if (this.isLoginMode) {
+    authObs = this.authService.login(email, password)
+
+  } else {
+    authObs = this.authService.signup(email, password);
+  }
+
+  authObs.subscribe(
+    resData => {
+      console.log(resData);
+      this.isLoading = false;
+      this.router.navigate(['/movies']);
+    },
+    errorMessage => {
+      console.log(errorMessage);
+     this.error = errorMessage;
+    //  this.showErrorAlert(errorMessage);
+      this.isLoading = false;
+    }
+  );
 
 form.reset();
 
